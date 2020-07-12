3a712804cd1cbfd667ada5dc6920b8d71b1cefc0
aargudo1910 <aargudo@espol.edu.ec>
2020-07-11 22:01:18 -0500

    Ajustes

--- JuegoMusicalChair/src/DoubleLinkedList/LinkedList.java
+++ JuegoMusicalChair/src/DoubleLinkedList/LinkedList.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CircularDoubleLinkedList;
+package DoubleLinkedList;
 
 /**
  *
  * @author Desarrollo
  */
-public class NodeList {
+public class LinkedList {
     
 }
