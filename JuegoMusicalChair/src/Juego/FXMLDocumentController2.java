bf4e444f50657c3049bf22efe6f714b54874fc17
aargudo1910 <aargudo@espol.edu.ec>
2020-07-27 22:59:47 -0500

    Correcciones

--- JuegoMusicalChair/src/Juego/FXMLDocumentController2.java
+++ JuegoMusicalChair/src/Juego/FXMLDocumentController2.java
@@ -14,7 +14,6 @@ import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
@@ -50,8 +49,6 @@ public class FXMLDocumentController2 extends Thread implements Initializable {
     private Label lblMusica;
     @FXML
     private Button btnEject;
-    @FXML
-    private Button btnMusica;
 
     @FXML
     private Pane PrincipalPane;
@@ -59,18 +56,16 @@ public class FXMLDocumentController2 extends Thread implements Initializable {
     private Button btnSigNivel;
     @FXML
     private ComboBox direcc;
-    @FXML
-    private Button btnDirec;
-    
+
     String direccion;
     int numPersonas;
+    Integer[] imgs = new Integer[20];
     Persona p = new Persona();
     Silla sil = new Silla();
     Musica mus = new Musica();
     DoubleCircularLinkedList<Persona> Personas;
     DoubleCircularLinkedList<Silla> Sillas;
-    HiloMovimiento hilo = new HiloMovimiento();
-           
+
     /**
      * Initializes the controller class.
      *
@@ -85,18 +80,6 @@ public class FXMLDocumentController2 extends Thread implements Initializable {
         direcc.setItems(list);
         direcc.setValue("Horario");
         TextPersonas.setText(null);
-        btnDirec.setVisible(false);
-    }
-
-    @FXML
-    private void musicaBtnAccion(ActionEvent event) {
-            if(lblMusica.getText().equals("ON")){
-                mus.getSound().stop();
-                lblMusica.setText("OFF");
-            }else{
-                mus.getSound().play();
-                lblMusica.setText("ON");
-            }  
     }
 
     @FXML
@@ -109,29 +92,27 @@ public class FXMLDocumentController2 extends Thread implements Initializable {
         String a = "Numero de personas: " + TextPersonas.getText();
         labelPersonas.setText(a);
         TextPersonas.setVisible(false);
-        btnDirec.setVisible(true);
         numPersonas = Integer.parseInt(TextPersonas.getText());
         Sillas = sil.ubicar(numPersonas);
-        
+
         Personas = p.ubicar(null, numPersonas, direccion, Sillas);
         for (int i = 0; i < numPersonas; i++) {
             try {
-                TimeUnit.MILLISECONDS.sleep(300);
+                TimeUnit.MILLISECONDS.sleep(50);
             } catch (InterruptedException ex) {
                 Logger.getLogger(FXMLDocumentController2.class.getName()).log(Level.SEVERE, null, ex);
             }
             PrincipalPane.getChildren().add(Personas.get(i).getImage());
         }
-    
+
         for (int i = 0; i < numPersonas - 1; i++) {
             PrincipalPane.getChildren().add(Sillas.get(i).getImage());
         }
-        
-        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(17), new EventHandler<ActionEvent>() {
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(19), new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
-                Integer[] imgs = new Integer[20];
                 for (int i = 0; i < numPersonas; i++) {
                     PrincipalPane.getChildren().remove(Personas.get(i).getImage());
                     imgs[i] = Personas.get(i).getNumeroImagen();
@@ -139,38 +120,69 @@ public class FXMLDocumentController2 extends Thread implements Initializable {
                 for (int i = 0; i < numPersonas - 1; i++) {
                     PrincipalPane.getChildren().remove(Sillas.get(i).getImage());
                 }
-
-                if (direccion.equals("Horario")) {
-                    direccion = "AntiHorario";
-                } else {
-                    direccion = "Horario";
-                }
-
-                numPersonas = numPersonas - 1;
-                Sillas = sil.ubicar(numPersonas);
-                System.out.println("imgs : " + imgs[0] + "  " + imgs[1] + "  " + imgs[2]);
-                Personas = p.ubicar(imgs, numPersonas, direccion, Sillas);
-                for (int i = 0; i < numPersonas; i++) {
-                    try {
-                        TimeUnit.MILLISECONDS.sleep(50);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(FXMLDocumentController2.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    PrincipalPane.getChildren().add(Personas.get(i).getImage());
-                }
-
-                for (int i = 0; i < numPersonas - 1; i++) {
-                    PrincipalPane.getChildren().add(Sillas.get(i).getImage());
-                }
-
+                btnSigNivel.setVisible(true);
             }
         }));
         timeline.setCycleCount(numPersonas - 2);
         timeline.play();
+
+        Timeline timeline2 = new Timeline(new KeyFrame(Duration.seconds(9), new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                mus.getSound().stop();
+                lblMusica.setText("OFF");
+            }
+        }));
+        timeline2.setCycleCount(1);
+        timeline2.play();
+
         mus.getSound().play();
         lblMusica.setText("ON");
+        System.out.println("ejecutar final");
+
+    }
+
+    @FXML
+    private void btnSiguiente(ActionEvent event) {
         
-        
+    btnSigNivel.setVisible(false);
+        if (direccion.equals("Horario")) {
+            direccion = "AntiHorario";
+            LabelDireccion.setText("AntiHorario");
+        } else {
+            direccion = "Horario";
+            LabelDireccion.setText("Horario");
+        }
+
+        numPersonas = numPersonas - 1;
+        Sillas = sil.ubicar(numPersonas);
+        System.out.println("imgs : " + imgs[0] + "  " + imgs[1] + "  " + imgs[2]);
+        Personas = p.ubicar(imgs, numPersonas, direccion, Sillas);
+        for (int i = 0; i < numPersonas; i++) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(50);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(FXMLDocumentController2.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            PrincipalPane.getChildren().add(Personas.get(i).getImage());
+        }
+
+        for (int i = 0; i < numPersonas - 1; i++) {
+            PrincipalPane.getChildren().add(Sillas.get(i).getImage());
+        }
+        mus.getSound().play();
+        lblMusica.setText("ON");
+        Timeline timeline3 = new Timeline(new KeyFrame(Duration.seconds(9), new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                mus.getSound().stop();
+                lblMusica.setText("OFF");
+            }
+        }));
+        timeline3.setCycleCount(1);
+        timeline3.play();
     }
 
     @FXML
@@ -189,44 +201,5 @@ public class FXMLDocumentController2 extends Thread implements Initializable {
         }
     }
 
-    @FXML
-    private void btnCambiarDir(ActionEvent event) {
-        
-    }
     
-    private class HiloMovimiento extends Thread {
-
-        Persona per;
-        int numeroPersonas;
-
-        @Override
-        public void run() {
-            System.out.println("runn hilo");
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException | IllegalArgumentException ex) {
-                System.out.println(ex.getMessage());
-            }
-            Platform.runLater(() -> {
-                try {
-                    Path p = per.createPath(per, numeroPersonas);
-                    PathTransition trans = new PathTransition(javafx.util.Duration.millis(10000), p, per.getImage());
-                    trans.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-                    trans.setCycleCount(5);
-                    trans.setDelay(Duration.seconds(2));
-                    trans.setRate(1);
-                    trans.playFrom(Duration.seconds(4));
-                    System.out.println("ejecutando");
-                } catch (NullPointerException ex) {
-                    System.out.println(ex.getMessage());
-                }
-            });
-
-        }
-
-        public void obtenerPersona(Persona per, int numeroPersonas) {
-            this.per = per;
-            this.numeroPersonas = numeroPersonas;
-        }
-    }
 }
